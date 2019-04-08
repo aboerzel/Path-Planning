733dbc7fe12f320dbaff24a696bc66dd768b9b9d
Andreas Börzel <andreas.boerzel@gmx.de>
2019-04-08 21:04:40 +0200

    fix: min distance for lane change increased to avoid collisions

--- src/HighwayDrivingBehavior.cpp
+++ src/HighwayDrivingBehavior.cpp
@@ -9,7 +9,7 @@ using namespace std;
 // parameter to adjust driving behavior
 #define MAX_SPEED 49.5                      // maximum speed in mph
 #define MIN_DISTANCE_TO_LEAD_VEHICLE 30.0   // minimum distance to the vehicle ahead in the same lane
-#define MIN_LANE_CHANGE_DISTANCE 5.0        // minimum distance to the vehicle ahead and behind on the target lane, needed for lane change
+#define MIN_LANE_CHANGE_DISTANCE 20.0       // minimum distance to the vehicle ahead and behind on the target lane, needed for lane change
 
 HighwayDrivingBehavior::HighwayDrivingBehavior()
 = default;
